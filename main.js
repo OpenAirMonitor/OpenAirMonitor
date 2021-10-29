1853af99d72bc81db71960a6fc88147c7cffc430
Gerrit Niezen <info@embeddedloop.com>
2021-10-29 16:39:15 +0100

    switch to CayenneLPP encoding

--- main.js
+++ main.js
@@ -123,6 +123,122 @@ const loraSetup = (init = false) => {
   });
 };
 
+const Ranges = {
+  DIGITAL_INPUT: [0, 255],
+  DIGITAL_OUTPUT: [0, 255],
+  ANALOG_INPUT: [-327.68, 327.67],
+  ANALOG_OUTPUT: [-327.68, 327.67],
+  LUMINOSITY: [0, 65535],
+  PRESENCE: [0, 1],
+  TEMPERATURE: [-3276.8, 3276.7],
+  RELATIVE_HUMIDITY: [0, 100],
+  ACCELEROMETER: [-32.768, 32.767],
+  MAGNETOMETER: [-32.768, 32.767],
+  BAROMETRIC_PRESSURE: [0, 6553.5],
+  GYROMETER: [-327.68, 327.67],
+  GPS_LATITUDE: [-90.0, 90.0],
+  GPS_LONGITUDE: [-180.0, 180.0],
+  GPS_ALTITUDE: [-3276.8, 3276.7],
+};
+
+const Scales = {
+  DIGITAL_INPUT: 1,
+  DIGITAL_OUTPUT: 1,
+  ANALOG_INPUT: 100,
+  ANALOG_OUTPUT: 100,
+  LUMINOSITY: 1,
+  PRESENCE: 1,
+  TEMPERATURE: 10,
+  RELATIVE_HUMIDITY: 2,
+  ACCELEROMETER: 1000,
+  MAGNETOMETER: 1000,
+  BAROMETRIC_PRESSURE: 10,
+  GYROMETER: 100,
+  GPS_LATITUDE: 10000,
+  GPS_LONGITUDE: 10000,
+  GPS_ALTITUDE: 100,
+};
+
+const Sizes = {
+  DIGITAL_INPUT: 1,
+  DIGITAL_OUTPUT: 1,
+  ANALOG_INPUT: 2,
+  ANALOG_OUTPUT: 2,
+  LUMINOSITY: 2,
+  PRESENCE: 1,
+  TEMPERATURE: 2,
+  RELATIVE_HUMIDITY: 1,
+  ACCELEROMETER: 6,
+  MAGNETOMETER: 6,
+  BAROMETRIC_PRESSURE: 2,
+  GYROMETER: 6,
+  GPS: 9,
+  CHANNEL_TYPE: 2,
+  UINT8: 1,
+  UINT16: 2,
+  INT16: 2,
+  THREE_AXIS: 6,
+};
+
+const Types = {
+  DIGITAL_INPUT: 0,
+  DIGITAL_OUTPUT: 1,
+  ANALOG_INPUT: 2,
+  ANALOG_OUTPUT: 3,
+  LUMINOSITY: 101,
+  PRESENCE: 102,
+  TEMPERATURE: 103,
+  RELATIVE_HUMIDITY: 104,
+  ACCELEROMETER: 113,
+  MAGNETOMETER: 114,
+  BAROMETRIC_PRESSURE: 115,
+  GYROMETER: 134,
+  GPS: 136,
+};
+
+const rangeCheck = (min, max, value) => {
+  if (value < min || value > max) {
+    throw new RangeError(`The argument must be between ${min} and ${max}.`);
+  }
+};
+
+const encodeChannelType = (channel, type) => {
+  const buffer = new ArrayBuffer(Sizes.CHANNEL_TYPE);
+  const dataView = new DataView(buffer);
+  let offset = 0;
+  [channel, type].forEach((value) => {
+    dataView.setInt8(offset, value);
+    offset += 1;
+  });
+  return buffer;
+};
+
+const encodeInt16 = function encodeInt16(value, scale) {
+  if (scale == null) {
+    scale = 1;
+  }
+  const buffer = new ArrayBuffer(Sizes.INT16);
+  const dataView = new DataView(buffer);
+  dataView.setInt16(0, (value * scale) | 0);
+  return buffer;
+};
+
+var concatBuffer = (buffer1, buffer2) => {
+  var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+  tmp.set(new Uint8Array(buffer1), 0);
+  tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+  return tmp.buffer;
+};
+
+const encodeAnalogInput = (channel, value) => {
+  const min = Ranges.ANALOG_INPUT[0];
+  const max = Ranges.ANALOG_INPUT[1];
+  rangeCheck(min, max, value);
+  const chanb = encodeChannelType(channel, Types.ANALOG_INPUT);
+  const snsb = encodeInt16(value, Scales.ANALOG_INPUT);
+  return concatBuffer(chanb, snsb);
+};
+
 lora.on('ready', () => {
   console.log('Finished setup, waiting for data..');
   Serial1.unsetup();
@@ -138,6 +254,7 @@ lora.on('ready', () => {
     const view = new DataView(buf);
 
     if (pmsData) {
+      /*
       view.setUint16(0, pmsData.dAtm.pm1);
       view.setUint16(2, pmsData.dAtm.pm2_5);
       view.setUint16(4, pmsData.dAtm.pm10);
@@ -147,6 +264,11 @@ lora.on('ready', () => {
       pmsData = null;
 
       const toSend = arrayBufferToHex(buf);
+      */
+
+      const pm10 = encodeAnalogInput(1, pmsData.dAtm.pm10);
+      const pm2_5 = encodeAnalogInput(2, pmsData.dAtm.pm2_5);
+      const toSend = arrayBufferToHex(pm10) + arrayBufferToHex(pm2_5);
 
       Serial1.println(`AT+MSGHEX="${toSend}"`);
       console.log(`Sending ${toSend}`);
